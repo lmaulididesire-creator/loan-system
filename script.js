3e7a0f598437ea0e75b1a5af9fe02a0a77e3fd8e
DESIRE MAULIDI <lmaulididesire@gmail.com>
2026-04-23 21:52:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+let total = 0;
+
+function calculate() {
+  let name = document.getElementById("name").value;
+  let amount = parseFloat(document.getElementById("amount").value);
+  let duration = parseInt(document.getElementById("duration").value);
+
+  if (!name || !amount || !duration) {
+    alert("Please fill all fields");
+    return;
+  }
+
+  let rate;
+  if (duration <= 1) {
+    rate = 0.20;
+  } else if (duration <= 4) {
+    rate = 0.25;
+  } else {
+    rate = 0.30;
+  }
+
+  total = amount + (amount * rate);
+
+  document.getElementById("result").innerText =
+    name + ", you will pay MK " + total;
+}
+
+function send() {
+  let name = document.getElementById("name").value;
+  let amount = document.getElementById("amount").value;
+  let duration = document.getElementById("duration").value;
+
+  if (!total) {
+    alert("Please check loan first");
+    return;
+  }
+
+  const formData = new URLSearchParams();
+  formData.append("name", name);
+  formData.append("amount", amount);
+  formData.append("duration", duration);
+  formData.append("total", total);
+
+  fetch("https://script.google.com/macros/s/AKfycbwzKhicpEOAbw6dybuX7LhmfM3Wrvi1f3Q23nRoZm9vMNjXc5AB86Tk7hgFeV3kKWdiaA/exec", {
+    method: "POST",
+    body: formData
+  })
+  .then(res => res.text())
+  .then(data => {
+    alert("Request sent successfully!");
+  })
+  .catch(err => {
+    alert("Error sending request");
+  });
+}
